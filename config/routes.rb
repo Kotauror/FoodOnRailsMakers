de9d3dbaf9843def7a3b3498816c22273df85c1c
kotpsot <zygmunt.justyna@gmail.com>
2018-03-13 12:17:34 +0000

    Created model and controller, first feature test passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  resources :restaurants
 
-  get '/' => 'restaurant#index'
-  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+# Prefix Verb   URI Pattern                     Controller#Action
+# restaurants GET    /restaurants(.:format)          restaurants#index
+#        POST   /restaurants(.:format)          restaurants#create
+# new_restaurant GET    /restaurants/new(.:format)      restaurants#new
+# edit_restaurant GET    /restaurants/:id/edit(.:format) restaurants#edit
+# restaurant GET    /restaurants/:id(.:format)      restaurants#show
+#        PATCH  /restaurants/:id(.:format)      restaurants#update
+#        PUT    /restaurants/:id(.:format)      restaurants#update
+#        DELETE /restaurants/:id(.:format)      restaurants#destroy
+# MacBook-Air-Kot:FoodOnRailsBeta2 kotpsot bin/rails server
--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -2,7 +2,7 @@ require_relative '../spec_helper'
 
 feature 'index page' do
   scenario 'when index page loads' do
-    visit('/')
+    visit('/restaurants')
     expect(page).to have_content("FoodOnRails - hello!!")
   end
 end
