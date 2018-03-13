cf50e99b0f5761d15c0ae9625225fd456ac6b55b
kotpsot <zygmunt.justyna@gmail.com>
2018-03-13 14:00:07 +0000

    Adjust feature test to the current headline

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -3,6 +3,6 @@ require_relative '../spec_helper'
 feature 'index page' do
   scenario 'when index page loads' do
     visit('/restaurants')
-    expect(page).to have_content("FoodOnRails - hello!!")
+    expect(page).to have_content("Food on Rails - a Rails application for Food")
   end
 end
