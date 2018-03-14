c0c1a7ff17cacc070d695793ac20117e2f924153
Laura Chan <laurachan205@gmail.com>
2018-03-14 12:44:43 +0000

    Modify restaurant view and reviews controller for test

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,18 @@
 class ReviewsController < ApplicationController
   def create
+    p params[:review]
     restaurant = Restaurant.find(params[:restaurant_id])
-    review = restaurant.reviews.new(params[:review])
-    review.save
+    p restaurant
+    review = Review.new(review_params)
+    restaurant.reviews << review
+    restaurant.save
     flash[:notice] = "Thanks #{review[:author]} review has been saved"
     redirect_to restaurant_path(restaurant)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:author, :text, :score)
   end
 end
