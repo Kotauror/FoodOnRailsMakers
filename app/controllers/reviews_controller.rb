155772528755157c8e8d1d8b27840600b0b96cff
Laura Chan <laurachan205@gmail.com>
2018-03-14 12:12:22 +0000

    Add create method to Review. Add POST route

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,9 @@
 class ReviewsController < ApplicationController
+  def create
+    restaurant = Restaurant.find(params[:restaurant_id])
+    review = restaurant.reviews.new(params[:review])
+    review.save
+    flash[:notice] = "Thanks #{review[:author]} review has been saved"
+    redirect_to restaurant_path(restaurant)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews, only: [:create]
+  end
+
   get '/' => 'restaurants#index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
