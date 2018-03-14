121c25bb4d5e0209c61e2de671e4639005812187
Laura Chan <laurachan205@gmail.com>
2018-03-14 11:44:44 +0000

    Add review controller. Add feature test for submitting review

--- spec/features/add_review_spec.rb
+++ spec/features/add_review_spec.rb
@@ -1,7 +1,20 @@
 feature 'add a review' do
-  scenario 'click on restaurants name takes us to restaurants page' do
+  scenario 'click on a restaurant name takes us to the restaurant page' do
     visit('/restaurants')
     click_on('DINO-GRILL')
     expect(page).to have_content("Dino Dinner was AMAZING")
   end
+
+  scenario 'submitting a review will display it on the restaurant page' do
+    visit('/restaurants')
+    click_on('DINO-GRILL')
+    fill_in('author').with('Laura')
+    fill_in('text').with('I was served the worst falafel with the rudest service')
+    fill_in('score').with(1)
+    click_on('submit')
+    expect(page).to have_content('I was served the worst falafel with the rudest service')
+  end
+
+# Add test for invalid form
+
 end
