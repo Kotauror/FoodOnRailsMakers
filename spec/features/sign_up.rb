ab0dcf38cb0aed6fbf25b8c78cebd9bc70d4d1ca
kotpsot <zygmunt.justyna@gmail.com>
2018-03-16 15:37:30 +0000

    successful sign up feature test passing

--- spec/features/sign_up.rb
+++ spec/features/sign_up.rb
@@ -1,10 +1,10 @@
 feature 'Sign up' do
   scenario 'Successful sign up' do
     visit('/')
-    click_button('Register')
-    p page.html
-    fill_in('user[email]', :with => 'myamazingworld@hello.com')
+    click_on('Register')
+    fill_in('user[email]', :with => 'myamazingworld@helloaa.com')
     fill_in('user[password]', :with => 'password')
+    fill_in('user[password_confirmation]', :with => 'password')
     click_on('Sign up')
     expect(page).to have_content('Welcome! You have signed up successfully.')
   end
